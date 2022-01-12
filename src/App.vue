cf3dff709adf8743dd8235230761899186a44c87
herman <semikozovgerman@gmail.com>
2022-01-12 14:21:57 +0200

    clean up App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,12 @@
 </template>
 
 <script>
-import Header from "./components/Header.vue";
-import Layout from "./components/Layout.vue";
-import Main from "./components/Main.vue";
-import PageNav from "./components/PageNav.vue";
 import CheckoutPage from "./pages/CheckoutPage.vue";
 
 export default {
   name: '16P Checkout',
   components: {
-    CheckoutPage,
-    PageNav,
-    Main,
-    Layout,
-    Header
+    CheckoutPage
   }
 }
 </script>
